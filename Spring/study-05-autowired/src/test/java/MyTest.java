97f74e95b9bbbf595332d62156319d9c0d85091c
aman2025 <2247367564@qq.com>
2021-04-23 11:23:44 +0800

    spring 章节ok

--- Spring/study-05-autowired/src/test/java/MyTest.java
+++ Spring/study-05-autowired/src/test/java/MyTest.java
@@ -3,7 +3,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class MyTest {
+public class .
+        MyTest {
     @Test
     public void test(){
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");

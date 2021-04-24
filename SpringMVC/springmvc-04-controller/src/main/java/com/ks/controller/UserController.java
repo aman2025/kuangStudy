f3b0b446451f82cc61be0051cbfb1505bccdbb9d
aman2025 <2247367564@qq.com>
2021-04-24 20:31:46 +0800

    04-controller; 05-json; ok

---
+++ SpringMVC/springmvc-04-controller/src/main/java/com/ks/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.ks.controller;
+
+import com.ks.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    // 前端请求 localhost:8080/user/t1?name=xxx
+    @RequestMapping("/t1")
+    public String test1(String name, Model model){
+        // 参数和前端的参数一致情况
+        model.addAttribute("msg", name);
+        return "hello";
+    }
+
+    // 前端请求 localhost:8080/user/t1?username=xxx
+    @RequestMapping("/t2")
+    public String test2(@RequestParam("username") String name, Model model){
+        // 参数和前端的参数不一致情况,用@RequestParam("username")
+        model.addAttribute("msg", name);
+        return "hello";
+    }
+
+    // 前端请求参数是一个i对象  localhost:8080/user/t1?id=1&name=xxx&age=3
+    @RequestMapping("/t3")
+    public String test3(User user, Model model){
+        System.out.println(user);
+        model.addAttribute("msg", user);
+        return "hello";
+    }
+}

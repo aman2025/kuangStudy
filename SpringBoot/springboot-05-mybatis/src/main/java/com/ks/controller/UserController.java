58f279e4e583de0fca48e647f9ddcf40ad9da5c3
aman2025 <2247367564@qq.com>
2021-05-07 21:10:56 +0800

    springboot-05-mybatis, 其他;

--- SpringBoot/springboot-05-mybatis/src/main/java/com/ks/controller/UserController.java
+++ SpringBoot/springboot-05-mybatis/src/main/java/com/ks/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ks.mapper.UserMapper;
 import com.ks.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,6 +14,10 @@ public class UserController {
     @Autowired
     UserMapper userMapper;
 
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+    }
     @GetMapping("/userList")
     public List<User> queryUserList(){
         List<User> userList = userMapper.queryUserList();
@@ -20,6 +25,16 @@ public class UserController {
             System.out.println(user);
         }
         return userList;
-
+    }
+    @GetMapping("/queryUser/{id}")
+    public User queryUserById(@PathVariable("id") int id){
+        User user = userMapper.queryUserById(id);
+        System.out.println(user);
+        return user;
+    }
+    @GetMapping("/deleteUser")
+    public String deleteUser(){
+        userMapper.deleteUser(5);
+        return "ok";
     }
 }
